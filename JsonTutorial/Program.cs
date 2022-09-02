001791f9bffbbf0866e7e0ba8cd1e2494e2c0b2d
Sam Schultz <sam.schultz@outlook.com>
2022-09-02 14:39:12 -0400

    Add project files.

---
+++ JsonTutorial/Program.cs
@@ -0,0 +1,31 @@
+﻿using JsonTutorial;
+using System.Text.Json;
+
+User noah = new User                                    //Universial Constructor
+{           
+    id = 1,
+    Username = "N.Phence",
+    Password = "Password",
+    FirstName = "Noah",
+    Lastname = "Phence",
+    Phone = "411",
+    Email = "N.Phence12@Gmail.com",
+    IsReviewer = true,
+    IsAdmin = false
+
+};
+
+JsonSerializerOptions options = new JsonSerializerOptions()
+{
+    PropertyNameCaseInsensitive = true,
+    WriteIndented = true
+
+};
+
+
+var json = JsonSerializer.Serialize(noah, options);
+
+User? noahConverted = JsonSerializer.Deserialize<User>(json);       //Deserialize convertes the json into a different type
+
+
+var v = 1;
